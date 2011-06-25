9f04194ef042870f3245e259a370380c17b7d1b3
Zach Kost-Smith <zachkostsmith@gmail.com>
2011-06-25 11:34:01 -0600

    Fixed the run-tests.sh script.  Library doesn't work with CMUCL (unexpected) or ABCL (expected)

--- run-tests.sh
+++ run-tests.sh
@@ -6,7 +6,7 @@ if which sbcl
 then
     sbcl <<EOF
 (ql:quickload :versioned-arrays-test)
-(-test:run-tests)
+(versioned-arrays-test:run-tests)
 (with-open-file (out #p"test-results" :if-exists :append :direction :output)
  (format out "sbcl: Success~%") )
 EOF
@@ -16,8 +16,8 @@ fi
 if which lisp
 then
     lisp <<EOF
-(ql:quickload :index-mapped-arrays-test)
-(ima-test:run-tests)
+(ql:quickload :versioned-arrays-test)
+(versioned-arrays-test:run-tests)
 (with-open-file (out #p"test-results" :if-exists :append :direction :output)
  (format out "cmucl: Success~%") )
 (quit)
@@ -27,8 +27,8 @@ fi
 if which ccl
 then
     ccl <<EOF
-(ql:quickload :index-mapped-arrays-test)
-(ima-test:run-tests)
+(ql:quickload :versioned-arrays-test)
+(versioned-arrays-test:run-tests)
 (with-open-file (out #p"test-results" :if-exists :append :direction :output)
  (format out "ccl: Success~%") )
 EOF
@@ -37,8 +37,8 @@ fi
 if which ccl64
 then
     ccl64 <<EOF
-(ql:quickload :index-mapped-arrays-test)
-(ima-test:run-tests)
+(ql:quickload :versioned-arrays-test)
+(versioned-arrays-test:run-tests)
 (with-open-file (out #p"test-results" :if-exists :append :direction :output)
  (format out "ccl64: Success~%") )
 EOF
@@ -47,8 +47,8 @@ fi
 if which clisp
 then
     clisp <<EOF
-(ql:quickload :index-mapped-arrays-test)
-(ima-test:run-tests)
+(ql:quickload :versioned-arrays-test)
+(versioned-arrays-test:run-tests)
 (with-open-file (out #p"test-results" :if-exists :append :direction :output)
  (format out "clisp: Success~%"))
 EOF
@@ -57,8 +57,8 @@ fi
 if which ecl
 then
     ecl <<EOF
-(ql:quickload :index-mapped-arrays-test)
-(ima-test:run-tests)
+(ql:quickload :versioned-arrays-test)
+(versioned-arrays-test:run-tests)
 (with-open-file (out #p"test-results" :if-exists :append :direction :output)
  (format out "ecl: Success~%"))
 EOF
@@ -67,8 +67,8 @@ fi
 if which abcl
 then
     abcl <<EOF
-(ql:quickload :index-mapped-arrays-test)
-(ima-test:run-tests)
+(ql:quickload :versioned-arrays-test)
+(versioned-arrays-test:run-tests)
 (with-open-file (out #p"test-results" :if-exists :append :direction :output)
  (format out "abcl: Success~%"))
 EOF
