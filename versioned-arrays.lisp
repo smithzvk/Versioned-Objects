ffa1def998f24672195b2fcc3e8b5b3b705b7ec3
Zach Kost-Smith <zachkostsmith@gmail.com>
2011-06-25 22:06:37 -0600

    Made the structure function names a bit more concise.  Easier to read and write.

--- versioned-arrays.lisp
+++ versioned-arrays.lisp
@@ -1,6 +1,6 @@
 
 (defpackage :versioned-arrays
-    (:use :cl :bt :iter :modf)
+  (:use :cl :bt :iter :modf)
   (:export #:make-versioned-array
            #:varef
            #:va-dimensions
@@ -8,15 +8,16 @@
 
 (in-package :versioned-arrays)
 
-(defstruct (versioned-array (:constructor %make-versioned-array))
+(defstruct (versioned-array (:constructor %make-versioned-array)
+                            (:conc-name :va-) )
   car cdr lock )
 
 (defun make-versioned-array (dimensions
                              &rest args
                              &key (element-type t)
-                             initial-element initial-contents
-                             adjustable fill-pointer
-                             displaced-to displaced-index-offset )
+                                  initial-element initial-contents
+                                  adjustable fill-pointer
+                                  displaced-to displaced-index-offset )
   "Make a versioned array."
   (declare (ignorable element-type initial-element initial-contents
                       adjustable fill-pointer displaced-to displaced-index-offset ))
@@ -32,57 +33,57 @@
 ;; delta at the same time.
 
 (defun varef (v-arr &rest idx)
-  (bt:with-lock-held ((versioned-array-lock v-arr))
+  (bt:with-lock-held ((va-lock v-arr))
     (raise-array! v-arr)
-    (apply #'aref (versioned-array-car v-arr) idx) ))
+    (apply #'aref (va-car v-arr) idx) ))
 
 (defun raise-array! (v-arr)
   "Bubble array to beginning of list, along the way back, reverse the list.
 This assumes that locks are already held."
-  (if (not (versioned-array-cdr v-arr))
+  (if (not (va-cdr v-arr))
       nil
       (progn
-        (raise-array! (versioned-array-cdr v-arr))
+        (raise-array! (va-cdr v-arr))
         (destructuring-bind (new-val &rest idx)
-            (versioned-array-car v-arr)
+            (va-car v-arr)
           ;; Move the array
-          (setf (versioned-array-car v-arr)
-                (versioned-array-car (versioned-array-cdr v-arr)) )
+          (setf (va-car v-arr)
+                (va-car (va-cdr v-arr)) )
           ;; Invert delta
-          (setf (versioned-array-car (versioned-array-cdr v-arr))
-                (cons (apply #'aref (versioned-array-car v-arr) idx) idx) )
+          (setf (va-car (va-cdr v-arr))
+                (cons (apply #'aref (va-car v-arr) idx) idx) )
           ;; Mutate array
-          (setf (apply #'aref (versioned-array-car v-arr) idx)
+          (setf (apply #'aref (va-car v-arr) idx)
                 new-val )
           ;; Reverse the list
-          (setf (versioned-array-cdr (versioned-array-cdr v-arr))
+          (setf (va-cdr (va-cdr v-arr))
                 v-arr )
           ;; Terminate the list
-          (setf (versioned-array-cdr v-arr) nil) ))))
+          (setf (va-cdr v-arr) nil) ))))
 
 (define-modf-function varef 1 (new-val v-arr &rest idx)
-  (bt:with-lock-held ((versioned-array-lock v-arr))
+  (bt:with-lock-held ((va-lock v-arr))
     (raise-array! v-arr)
-    (let* ((arr (versioned-array-car v-arr))
+    (let* ((arr (va-car v-arr))
            (old-value (apply #'aref arr idx)) )
       (setf (apply #'aref arr idx) new-val)
-      (setf (versioned-array-cdr v-arr) (%make-versioned-array
-                                         :car arr
-                                         :lock (versioned-array-lock v-arr) )
-            (versioned-array-car v-arr) (cons old-value idx) ))
-    (versioned-array-cdr v-arr) ))
+      (setf (va-cdr v-arr) (%make-versioned-array
+                            :car arr
+                            :lock (va-lock v-arr) )
+            (va-car v-arr) (cons old-value idx) ))
+    (va-cdr v-arr) ))
 
 ;; Some niceties...
 
 (defun va-dimensions (v-arr)
-  (bt:with-lock-held ((versioned-array-lock v-arr))
+  (bt:with-lock-held ((va-lock v-arr))
     (raise-array! v-arr)
-    (array-dimensions (versioned-array-car v-arr)) ))
+    (array-dimensions (va-car v-arr)) ))
 
 (defun va-dimension (v-arr n)
   (nth n (va-dimensions v-arr)) )
 
 (defmethod print-object ((obj versioned-array) str)
-  (bt:with-lock-held ((versioned-array-lock obj))
+  (bt:with-lock-held ((va-lock obj))
     (raise-array! obj)
-    (print (versioned-array-car obj) str) ))
+    (print (va-car obj) str) ))
