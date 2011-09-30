54469876da5afc50d382f7fadc025ccf8ac10835
Zach Kost-Smith <zachkostsmith@gmail.com>
2011-09-30 09:45:35 -0600

    Changed the workings of VACCESS so that it uses VFUNCALL instead of WITH-VERSIONED-OBJECT.  This makes it considerably more robust.

--- versioned-objects.lisp
+++ versioned-objects.lisp
@@ -401,11 +401,12 @@ list."
 ;;<<>>=
 (defmacro vaccess (form)
   "The macro vaccess provides a slightly less annoying way to access a value
-from a versioned-object.  It is really just a shorthand for
-WITH-VERSIONED-OBJECT(S)."
-  (let ((cont-sym (find-container form)))
-    `(with-versioned-object ,cont-sym
-       ,form )))
+from a versioned-object.  This exands the access form by littering it with
+VFUNCALLs."
+  (if (atom form)
+      form
+      (list* 'vfuncall (list 'quote (first form))
+             (mapcar (lambda (x) (macroexpand (list 'vaccess x))) (rest form)) )))
 
 ;; @\section{Printing}
 
